22e966743639f45da8c50365542ab76a90f4e5d5
t-yng <t.yng.greenmoneky@gmail.com>
2017-08-06 17:42:14 +0900

    SPAの枠組みを作成

--- public/app.js
+++ public/app.js
@@ -1,24 +1,113 @@
 'use strict';
 
-let learnjs = (() => {
-  return {
-    problemView: (number) => {
-      const title = `Problem #${number} Coming soon!`;
-      return $('<div class="problem-view">').text(title);
+(function(global){
+  let learnjs = {
+    applyObject: (obj, view) => {
+      for(const key in obj) {
+        view.find(`[data-name="${key}"]`).text(obj[key]);
+      }
     },
+
+    problemView: (number) => {
+      number = Number(number);
+      let view = $('.templates .problem-view').clone();
+      const problem = learnjs.problems[number-1];
+      const title = `Problem #${number}`;
+      const resultFlush = view.find('.result');
+
+      const checkAnswer = () => {
+        const answer = view.find('.answer').val();
+        const test = problem.code.replace('__', answer) + '; problem();';
+        return eval(test);
+      }
+
+      const onClickCheckAnswer = () => {
+        if(checkAnswer()) {
+          const correctFlush = learnjs.buildCorrectFlush(number);
+          learnjs.flushElement(resultFlush, correctFlush);
+        } else {
+          learnjs.flushElement(resultFlush, 'Incorrect!');
+        }
+
+        return false;
+      }
+
+      view.find('.check-btn').on('click', onClickCheckAnswer);
+      view.find('.title').text(title);
+      learnjs.applyObject(problem, view);
+
+      if(number < learnjs.problems.length) {
+        const skipButton = learnjs.templates('skip-btn');
+        skipButton.find('a').attr('href', `#problem-${number+1}`);
+        $('.nav-list').append(skipButton);
+        view.bind('removingView', () => skipButton.remove());
+      }
+
+      return view;
+    },
+
+    landingView: () => {
+      return learnjs.templates('landing-view');      
+    },
+
     showView: (hash) => {
       const routes = {
+        '': learnjs.landingView,
+        '#': learnjs.landingView,
         '#problem': learnjs.problemView
       }
       const hashParts = hash.split('-');
       const viewFnc = routes[hashParts[0]];
       if(viewFnc) {
+        learnjs.triggerEvent('removingView', []);
         $('.view-container').empty().append(viewFnc(hashParts[1]));
       }
     },
+
     appOnReady: () => {
       window.onhashchange = () => learnjs.showView(window.location.hash);
       learnjs.showView(window.location.hash);
+    },
+
+    buildCorrectFlush: (problemNumber) => {
+      const correctFlush = learnjs.templates('correct-flush');
+      const link = correctFlush.find('a');
+      if(problemNumber < learnjs.problems.length) {
+        link.attr('href', `#problem-${problemNumber+1}`);
+      } else {
+        link.attr('href', '');
+        link.text('You\'re Fnished!');
+      }
+
+      return correctFlush;
+    },
+
+    flushElement: (elem, content) => {
+      elem.fadeOut('fast', () => {
+        elem.html(content);
+        elem.fadeIn();
+      });
+    },
+
+    templates: name => {
+      return $(`.templates .${name}`).clone();
+    },
+
+    triggerEvent: (event, args) => {
+      $('.view-container>*').trigger(event, args);
     }
   }
-})();
+
+  learnjs.problems = [
+    {
+      description: 'What is truth?',
+      code: 'function problem() { return __; }'
+    },
+    {
+      description: 'Simple Math?',
+      code: 'function problem() { return 42 === 6 * __; }'
+    }
+  ];
+
+  global.learnjs = learnjs;
+})(this);
--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -26,12 +26,66 @@ describe('LearnJS', () => {
     spyOn(learnjs, 'showView');
     $(window).trigger('hashchange');
     expect(learnjs.showView).toHaveBeenCalledWith(window.location.hash);
-  })
+  });
+
+  it('invisible skip button in landing page', () => {
+    const view = learnjs.landingView();
+    expect($('.nav-container .skip-btn').length).toEqual(0);
+  });
 
   describe('problem view', () => {
+
+    beforeEach(() => {
+      this.view = learnjs.problemView('1');
+    })
+
     it('has a title that includes the problem number', () => {
-      const view = learnjs.problemView('1');
-      expect(view.text()).toEqual('Problem #1 Coming soon!');
+      expect(view.find('.title').text()).toEqual('Problem #1');
+    });
+
+    it('show the description', () => {
+      expect(view.find('[data-name="description"]').text()).toEqual('What is truth?');
+    });
+
+    it('show the problem code', () => {
+      expect(view.find('[data-name="code"]').text()).toEqual('function problem() { return __; }');
+    });
+
+    it('show the skip button', () => {
+      expect($('.nav-container .skip-btn').length).toEqual(1);
+    });
+  });
+
+  describe('answer section', () => {
+    beforeEach(() => {
+      this.view = learnjs.problemView('1');
+      this.answerCorrect = () => {
+        view.find('.answer').val('true');
+        view.find('.check-btn').click();
+      }
+    });
+
+    it('can check a correct answer by hitting a button', () => {
+      answerCorrect();
+      expect(view.find('.correct-flush a').length).toEqual(1);
+    });
+
+    it('can move next problem after answer correct', () => {
+      const correctFlush = learnjs.buildCorrectFlush(1);
+      const link = correctFlush.find('a');
+      expect(link.attr('href')).toEqual('#problem-2');
+    });
+
+    it('move landing page after answer last problem', () => {
+      const correctFlush = learnjs.buildCorrectFlush(learnjs.problems.length);
+      const link = correctFlush.find('a');
+      expect(link.attr('href')).toEqual('');
+    });
+
+    it('rejects an incorrect answer', () => {
+      view.find('.answer').val('false');
+      view.find('.check-btn').click();
+      expect(view.find('.result').text()).toEqual('Incorrect!');
     });
   });
 });
